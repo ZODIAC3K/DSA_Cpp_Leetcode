c6e135ae746db04dcd0d4a8ce8f4f525fa9269f3
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-06-04 15:36:05 +0530

    Time: 8 ms (34.60%) | Memory: 8.6 MB (83.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,13 +4,21 @@ public:
         int start=0;
         int end=s.size()-1;
         while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
+            if(!isalnum(s[start])){ // this helps us skip space and special character from start
+                start++; 
+                continue;
+            }
+            if(!isalnum(s[end])){ // this helps us skip space and special character from back
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end])){
+                return false;
+            }
+            else{
                start++;
                end--;
-           }
+            }
        }
        return true;
     }
