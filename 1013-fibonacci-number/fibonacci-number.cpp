9bf28e977d839dd12cc43710b7d1cb2d6fe75182
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-06-04 15:51:20 +0530

    Time: 11 ms (44.68%) | Memory: 7.2 MB (12.21%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 2 && n != 0){
+            return 1;
+        }   
+        if(n == 0){
+            return 0;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+};
