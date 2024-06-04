9bb95164cb9ffa6c720ded073d49b2c9155c8869
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-06-04 15:29:11 +0530

    Time: 4 ms (80.03%) | Memory: 8.6 MB (61.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // if(s.size() == 1){
-        //     return true;
-        // }
-        for(int i = 0; i < s.size(); i++){
-            if(isdigit(s[i])){
-                continue;
-            }
-            else if(isalpha(s[i])){
-                s[i] = tolower(s[i]);
-            }
-            else if (!isalpha(s[i]) || s[i] == ' '){
-                s.erase(s.begin()+i);
-                i--;
-            }
-        }
-
-        bool status = true;
-        int start = 0;
-        int end = s.size()-1;
-
-        // trying two pointer aproach basic...
-        while(start <= end){
-            if(s[start] != s[end]){
-                status = false;
-                return status;
-            }
-            else{
-                start++;
-                end--;
-            }
-        }
-    
-        return status;
+        int start=0;
+        int end=s.size()-1;
+        while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
     }
 };
