1ab0322a3a70168e3ba959881c4b33969e776b22
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-06-30 22:31:21 +0530

    Time: 39 ms (39.78%) | Memory: 16.6 MB (25.85%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,13 +1,18 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         number = 0
-
+        
+        # Make a number from given digits
         for nums in digits:
             number = number * 10 + nums
 
+        # Added a 1 to the number
         number += 1
 
+        # Didn't want to use extra space for array so used "digits" (by default array is passed by reference) by make it empty
         digits = []
+
+        # read last digit from number using "number % 10" and pushed in simple in digits if the len of digits is 0 else inserted it in front of digits and then removed last digit from number using "number // 10" and return the new digits
 
         while(number > 0):
             last_digit = number % 10
