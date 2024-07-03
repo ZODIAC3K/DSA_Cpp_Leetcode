802d22662360f76f41565a19238b0ea45b73cfe3
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-07-03 20:25:20 +0530

    Time: 120 ms (27.73%) | Memory: 17.6 MB (86.81%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # TC --> O(n*logn)
 
+        #edge case
         if len(nums) == 1:
             if nums[0] == 1:
                 return 0
@@ -11,7 +13,7 @@ class Solution:
         max_range = len(nums)
 
         nums.sort()
-
+        #edge case
         if nums[0] != 0:
             return 0
 
