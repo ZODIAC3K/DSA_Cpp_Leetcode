c6d8cbe59f062a6e4d458f88e360edef2dd4831a
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-07-03 14:15:00 +0530

    Time: 167 ms (72.93%) | Memory: 18 MB (55.69%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = {}
-        for element in nums:
-            hashmap[element] = hashmap.get(element, 0) + 1
+        # hashmap = {}
+        # for element in nums:
+        #     hashmap[element] = hashmap.get(element, 0) + 1
         
-        for key, value in hashmap.items():
-            if value > len(nums)/2:
-                return key
+        # for key, value in hashmap.items():
+        #     if value > len(nums)/2:
+        #         return key
+
+        ###
+        ### OR
+        ###
+
+        nums.sort()
+        n = len(nums)
+        return nums[n//2]
