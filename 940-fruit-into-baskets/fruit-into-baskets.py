34eb119ff6af63ff9aae788a98197a231339141f
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-07-04 16:53:14 +0530

    Time: 678 ms (52.55%) | Memory: 23.8 MB (78.02%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,27 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l = 0
+        r = 0
+        maxlen = 0
+        basket = dict()
+        
+        while r < len(fruits):
+            basket[fruits[r]] = basket.get(fruits[r], 0) + 1
+            
+            if len(basket) <= 2:
+                maxlen = max(maxlen, r - l + 1)
+            
+            while len(basket) > 2:
+
+                basket[fruits[l]] -= 1
+
+                if basket[fruits[l]] == 0:
+                    del basket[fruits[l]]
+                    
+                l += 1
+            
+            r += 1
+
+        return maxlen
+
+        
