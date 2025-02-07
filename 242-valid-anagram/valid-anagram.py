781385ad1241c0da91ca3d556df6085eb6da4865
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2025-02-08 01:32:17 +0530

    Time: 15 ms (43.45%) | Memory: 18.5 MB (10.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Sort both strings. If they are anagrams, their sorted forms will be identical else they will differ. But sorting returns a list which requires extra space that makings this approach sub-optimal.
         s = sorted(s)
         t = sorted(t)
         if s == t:
