74b670b6adb11d7f3a7c55292a016eb6f49b3961
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2025-02-08 01:16:10 +0530

    Time: 18 ms (20.86%) | Memory: 35 MB (9.48%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,16 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        saw = set()
+        # saw = set()
+        # for e in nums:
+        #     if e in saw:
+        #         return True
+        #     saw.add(e)
+        # return False
+
+        saw = {}
         for e in nums:
             if e in saw:
                 return True
-            saw.add(e)
+            saw[e] = saw.get(e, 0) + 1
         return False
-
-        # saw = {}
-        # for e in nums:
-        #     if e in saw and saw[e] >= 1:
-        #         return True
-        #     saw[e] = saw.get(e, 0) + 1
-        # return False
