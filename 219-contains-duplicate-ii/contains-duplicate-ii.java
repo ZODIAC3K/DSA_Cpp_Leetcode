ae921633e2258c364ea286c5c3a53c75daceba75
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2025-11-18 15:10:07 +0530

    Time: 30 ms (12.46%) | Memory: 89.7 MB (13.33%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -4,26 +4,40 @@ class Solution {
         // 1 → [0, 3]
         // 2 → [1, 4]
         // 3 → [2, 5]
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+    //     HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (!map.containsKey(nums[i])) {
+    //             ArrayList<Integer> list = new ArrayList<>();
+    //             list.add(i);
+    //             map.put(nums[i], list); // adds new arraylist with i in it and put it in map with key
+    //             continue;
+    //         }
+
+    //         map.get(nums[i]).add(i);
+
+    //         ArrayList<Integer> data = map.get(nums[i]);
+    //         int secondLast = data.get(data.size() - 2);
+
+    //         if (Math.abs(i - secondLast) <= k) {
+    //             // because most recent will be bigger but we have abs so not required to be honest.
+    //             return true;
+    //             // returns for any value which full fills above if condition.
+    //         }
+
+    //     }
+    //     return false;
+    // }
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (!map.containsKey(nums[i])) {
-                ArrayList<Integer> list = new ArrayList<>();
-                list.add(i);
-                map.put(nums[i], list); // adds new arraylist with i in it and put it in map with key
+                map.put(nums[i], i);
                 continue;
             }
-
-            map.get(nums[i]).add(i);
-
-            ArrayList<Integer> data = map.get(nums[i]);
-            int secondLast = data.get(data.size() - 2);
-
-            if (Math.abs(i - secondLast) <= k) {
-                // because most recent will be bigger but we have abs so not required to be honest.
+            int lastValue = map.get(nums[i]);
+            if (Math.abs(i - lastValue) <= k) {
                 return true;
-                // returns for any value which full fills above if condition.
             }
-
+            map.put(nums[i], i);
         }
         return false;
     }
